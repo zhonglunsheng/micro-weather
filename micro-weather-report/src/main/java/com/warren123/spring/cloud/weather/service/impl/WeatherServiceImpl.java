46f163a66d2b986456fe9fe4728821636d312263
zhonglunsheng <873946453@qq.com>
2018-07-02 09:20:37 +0800

    集成redis缓存

--- micro-weather-report/src/main/java/com/warren123/spring/cloud/weather/service/impl/WeatherServiceImpl.java
+++ micro-weather-report/src/main/java/com/warren123/spring/cloud/weather/service/impl/WeatherServiceImpl.java
@@ -6,16 +6,15 @@ import com.warren123.spring.cloud.weather.vo.WeatherResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
 
 @Service("weatherService")
 public class WeatherServiceImpl implements WeatherService{
@@ -30,9 +29,18 @@ public class WeatherServiceImpl implements WeatherService{
      */
     private  static final Integer API_STATUS = 200;
 
+    /**
+     * 缓存时间
+     */
+    private static final Long  TIME_OUT = 1800L;
+
     private Logger log = LoggerFactory.getLogger(WeatherServiceImpl.class);
+
     @Autowired
     RestTemplate restTemplate;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
 
     @Override
     public WeatherResponse getDataByCityId(String id) {
@@ -55,23 +63,36 @@ public class WeatherServiceImpl implements WeatherService{
 
 
     private WeatherResponse doGetWeather(String url) {
-        ResponseEntity<String> repString =  restTemplate.getForEntity(url, String.class);
         ObjectMapper mapper = new ObjectMapper();
         WeatherResponse response = null;
         String strBody = null;
+        String key = url;
 
-        if (repString.getStatusCodeValue() == API_STATUS) {
-            strBody = repString.getBody();
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+
+        //判断缓存是否有数据
+        if (stringRedisTemplate.hasKey(key)) {
+            log.info("redis has data");
+            strBody = ops.get(key);
+        } else {
+            ResponseEntity<String> repString = restTemplate.getForEntity(url, String.class);
+
+            if (repString.getStatusCodeValue() == API_STATUS) {
+                strBody = repString.getBody();
+            }
+            log.info("redis has no data");
+            //缓存数据
+            ops.set(key, strBody, TIME_OUT, TimeUnit.SECONDS);
         }
 
         try {
             // Json数据转Object对象
             response = mapper.readValue(strBody, WeatherResponse.class);
         } catch (IOException e) {
-            log.warn("Json turn error");
+            log.error("Json turn error");
             e.printStackTrace();
         }
-
         return response;
     }
+
 }
